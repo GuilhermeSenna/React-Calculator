434104167b6cd9e25a3ee3bfd6dd0983eb41fbf4
zBiggui <bigguisenna@gmail.com>
2022-06-09 18:53:06 -0300

    Updates

---
+++ precache-manifest.fc8c6a0664279ecd1c144fc74e7f6b12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81172cb9785baa81ae54",
+    "url": "/React-Calculator/static/css/main.24780e3d.chunk.css"
+  },
+  {
+    "revision": "81172cb9785baa81ae54",
+    "url": "/React-Calculator/static/js/main.81172cb9.chunk.js"
+  },
+  {
+    "revision": "f88341d7d2264370803c",
+    "url": "/React-Calculator/static/js/1.f88341d7.chunk.js"
+  },
+  {
+    "revision": "4a9b6dfe2c0ed41fbe05",
+    "url": "/React-Calculator/static/js/runtime~main.4a9b6dfe.js"
+  },
+  {
+    "revision": "3075baac906d2bc4f56abda98119b3b0",
+    "url": "/React-Calculator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-Calculator/precache-manifest.fc8c6a0664279ecd1c144fc74e7f6b12.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-Calculator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
